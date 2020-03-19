7a481595343c5bdca306275ebce91974ca7f269e
Jake Hannan <jake.hannan@slalom.com>
2020-03-19 11:16:39 -0400

    added distinct orders

--- models/customers.sql
+++ models/customers.sql
@@ -19,6 +19,7 @@ with customers as (
         , min(order_date) as first_order_date
         , max(order_date) as most_recent_order_date
         , count(order_id) as number_of_orders
+        , count(distinct order_id) as distinct_orders
     from orders
     group by 1
 )
@@ -31,6 +32,7 @@ with customers as (
         , customer_orders.first_order_date
         , customer_orders.most_recent_order_date
         , coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        , coalesce(customer_orders.distinct_orders, 0) as distinct_orders
     from customers
 
     left join customer_orders using (customer_id)
